b8df1a582c476d1a9b0e02251bb27b0b3d9b69a0
Tetiana Tatsii <itsmetetiana@gmail.com>
2026-06-07 10:48:05 +0300

    feat: add validation, pagination, and filtering for notes

--- src/models/note.js
+++ src/models/note.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import { TAGS } from '../constants/tags.js';
 
 const noteSchema = new mongoose.Schema(
   {
@@ -14,19 +15,7 @@ const noteSchema = new mongoose.Schema(
     },
     tag: {
       type: String,
-
-      enum: [
-        'Work',
-        'Personal',
-        'Meeting',
-        'Shopping',
-        'Ideas',
-        'Travel',
-        'Finance',
-        'Health',
-        'Important',
-        'Todo',
-      ],
+      enum: TAGS,
       default: 'Todo',
     },
   },
@@ -35,5 +24,7 @@ const noteSchema = new mongoose.Schema(
     versionKey: false,
   },
 );
+
+noteSchema.index({ tag: 1 });
 
 export const Note = mongoose.model('Note', noteSchema);
--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { errors } from 'celebrate';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
@@ -19,6 +20,8 @@ app.use(logger);
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
+
+app.use(errors());
 
 app.use(errorHandler);
 
